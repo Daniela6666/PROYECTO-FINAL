a52c50d60f34893193b38ebbaaa510eaba3eb53f
Daniela6666 <DANIELA.MOLINA-L@mail.escuelaing.edu.co>
2024-05-23 18:38:28 -0500

    Corrección aplicación consola

---
+++ app_consola/build/CMakeFiles/ejecutable_consola.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejecutable_consola.dir/src/control_db.cpp.o"
+  "CMakeFiles/ejecutable_consola.dir/src/control_db.cpp.o.d"
+  "CMakeFiles/ejecutable_consola.dir/src/main.cpp.o"
+  "CMakeFiles/ejecutable_consola.dir/src/main.cpp.o.d"
+  "CMakeFiles/ejecutable_consola.dir/src/sensor.cpp.o"
+  "CMakeFiles/ejecutable_consola.dir/src/sensor.cpp.o.d"
+  "ejecutable_consola"
+  "ejecutable_consola.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejecutable_consola.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
